777705cc509a4f911549b3f1af5651bfdd944db2
MitchelMA <32548@ma-web.nl>
2022-04-05 14:07:00 +0200

    heatseeking projectile wip

--- Assets/Scripts/Heatseeking/projectile.cs
+++ Assets/Scripts/Heatseeking/projectile.cs
@@ -21,11 +21,14 @@ public class projectile : MonoBehaviour
 
     [SerializeField] 
     private Transform seekTarget;
+
+    [SerializeField] private float fieldOfView = 45;
+    
+    [SerializeField]
+    private Vector3 velocity;
     #endregion
 
     #region Privates
-    [SerializeField]
-    private Vector3 velocity = Vector3.zero;
     #endregion
 
     // Start is called before the first frame update
@@ -36,38 +39,69 @@ public class projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        seek(seekTarget.position);
+        // seek the target
+        seek(seekTarget.position, fieldOfView);
     }
 
-    private void seek(Vector3 target)
+    private void seek(Vector3 target, float FOV)
     {
+        // calculate the desired velocity
         Vector3 desired = target - transform.position;
         desired = SetMag(desired, speed * Time.deltaTime);
-        Vector3 steering = desired - velocity;
+        
+        // the angle between the target and the velocity
+        float angle = Vector3.Angle(velocity, desired);
+        
+        // steering vector
+        Vector3 steering;
+        
+        // see if the angle between target and velocity is greater or lesser than the FOV
+        if (angle > FOV)
+            steering = Vector3.zero;
+        else
+            steering = desired - velocity;
+        
+        // limit the steering vector and apply it to the velocity
         steering = Limit(steering, maxForce * Time.deltaTime);
         ApplyForce(steering);
     }
 
     private void ApplyForce(Vector3 force)
     {
+        // add the force to the velocity
         velocity += force;
+        // limit the velocity bij the max speed
         velocity = Limit(velocity, speed * Time.deltaTime);
+        // calculate the new direction
         Vector3 newDir = Vector3.RotateTowards(transform.eulerAngles, velocity, 2 * Mathf.PI, 2*maxForce);
+        // rotate according to this new direction
         transform.rotation = Quaternion.LookRotation(newDir);
+        // translate forwards
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
     
     
     
-    // Vector Methods
+    /// <summary>
+    /// Method to set the magnitude of a vector
+    /// </summary>
+    /// <param name="vector">Input vector</param>
+    /// <param name="mag">New magnitude</param>
+    /// <returns>A vector3 with the new magnitude</returns>
     private Vector3 SetMag(Vector3 vector, float mag)
     {
+        // 
         if (vector.magnitude == 0) return vector;
         vector.Normalize();
         vector *= mag;
         return vector;
     }
-
+    /// <summary>
+    /// Method to limit a vector with a certain magnitude
+    /// </summary>
+    /// <param name="vector">Input vector</param>
+    /// <param name="limit">Maximum magnitude</param>
+    /// <returns>A vector of which the magnitude cannot be greater than the specified limit</returns>
     private Vector3 Limit(Vector3 vector, float limit)
     {
         if (vector.magnitude == 0 || vector.magnitude < limit) return vector;
